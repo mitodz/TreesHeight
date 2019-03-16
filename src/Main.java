8aa77278e38d3c7d3fa1cdff0a8af9e203fda96a
mitodz <mitodzzz@gmail.com>
2019-03-16 21:52:38 +0300

    Совершенно не по лекции)

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,15 @@ public class Main implements Runnable{
         for (int i = 0; i < n; i++) { //заполняем первоначальный массив родителей
             a[i] = scanner.nextInt();
         }
-        for (int i = 0; i < n; i++) { // далее создаем массив из детей (дети в индексах) со списками родителей в ячейках
+        height = getHeight(a);
+        System.out.println(height + 1);
+    }
+
+    private static int getHeight(int[] a) {
+        int n = a.length;
+        int[] p = new int[n];
+        int height = 0;
+        for (int i = 0; i < n; i++) { // далее создаем массив (дети-индексы) со счетчиками родителей в ячейках
             int t = i;
             while(a[t]!=-1) {
                 p[i]++;
@@ -22,8 +30,7 @@ public class Main implements Runnable{
                 t=a[t];
             }
         }
-
-        System.out.println(height + 1);
+        return height;
     }
 
     public static void main(String[] args) {
